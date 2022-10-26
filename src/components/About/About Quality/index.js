37bdebba469308bd9216190271b86b6ffd215c19
Derick McMillian <dmvmills@icloud.com>
2022-10-26 04:42:49 -0500

    completed 'about quality' w/ responsiveness

---
+++ src/components/About/About Quality/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+const AboutQuality = () => {
+  return (
+    <section className="about-quality-container">
+      <div className="about-quality-text">
+        <h3 className="about-quality-title">Uncompromising quality</h3>
+        <p className="about-quality-description">
+          Although we work with growers who pay close attention to all stages of
+          harvest and processing, we employ, on our end, a rigorous quality
+          control program to avoid over-roasting or baking the coffee dry. Every
+          bag of coffee is tagged with a roast date and batch number. Our goal
+          is to roast consistent, user-friendly coffee, so that brewing is easy
+          and enjoyable.
+        </p>
+      </div>
+      <div className="about-quality-image"></div>
+    </section>
+  );
+};
+export default AboutQuality;
--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -1,5 +1,6 @@
 import AboutCommitment from "../../components/About/About Commitment";
 import AboutHeroCard from "../../components/About/About Hero Card";
+import AboutQuality from "../../components/About/About Quality";
 import Layout from "../../layout";
 
 function AboutUs() {
@@ -7,6 +8,7 @@ function AboutUs() {
     <Layout>
       <AboutHeroCard />
       <AboutCommitment />
+      <AboutQuality />
     </Layout>
   );
 }
